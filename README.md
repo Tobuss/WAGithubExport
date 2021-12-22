80708b7e9d2d97b6643b60761cd60943f1ca2544
Alex <zuedev@gmail.com>
2021-12-22 21:14:42 +0000

    initial upload for testing

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # WAGithubExport
+
 A self-updating GitHub repository for backing up World Anvil worlds.
+
+## How to use
+
+Just fork the repository and create the following two secrets:
+
+- `WA_AUTH_TOKEN`: Your World Anvil API key. You can get one [here](https://www.worldanvil.com/api/auth/key).
+- `WA_APP_KEY`: Your World Anvil application key. You get this from Dimitris right now.
+
+Your repository should start backing up your worlds every day at midnight.
+
+## TODO
+
+- [ ] Initial functionality
+- [ ] Include user metadata (bio, avatar, etc.)
+- [ ] Include world metadata (intro, world date, etc.)
+- [ ] Include more than articles (timelines, maps, images, etc.)
+- [ ] Selective backups
---
+++ main.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import fs from "fs";
+
+const instance = axios.create({
+  baseURL: "https://www.worldanvil.com/api/aragorn",
+  headers: {
+    "x-auth-token": process.env.WA_AUTH_TOKEN,
+    "x-application-key": process.env.WA_APP_KEY,
+    "Content-Type": "application/json",
+    "User-Agent": "WAGitExport ( https://some.url, 0.0.0 )",
+  },
+});
+
+if (!process.env.WA_AUTH_TOKEN || !process.env.WA_APP_KEY) {
+  throw new Error("Error: Credentials not found");
+} else {
+  fs.rm("./export", { recursive: true, force: true }, () => {
+    instance.get("/user").then((res) => {
+      instance.get(`/user/${res.data.id}/worlds`).then((res) => {
+        res.data.worlds.forEach((world) => {
+          instance.get(`/world/${world.id}/articles`).then((res) => {
+            res.data.articles.forEach((article) => {
+              instance.get(`/article/${article.id}`).then((res) => {
+                fs.mkdir(
+                  `./export/${world.name}/${article.template_type}`,
+                  { recursive: true },
+                  () => {
+                    fs.writeFileSync(
+                      `./export/${world.name}/${article.template_type}/${article.title}.json`,
+                      JSON.stringify(res.data, null, 2)
+                    );
+                  }
+                );
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+}
